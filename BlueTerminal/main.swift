0aeafa91bd05b384afdd2c0f2f7238c23e083595
tctsigemura <sigemura@tokuyama.ac.jp>
2019-03-24 21:24:39 +0900

    Initial Commit

---
+++ BlueTerminal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BlueTerminal
+//
+//  Created by 重村哲至 on 2019/03/24.
+//  Copyright © 2019 重村哲至＠徳山高専. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
